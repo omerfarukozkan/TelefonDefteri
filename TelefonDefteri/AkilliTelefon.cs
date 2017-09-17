dff4659bb207370028c8155de3cb415aa2d1542e
omerfarukozkan <omrfrkozkn@gmail.com>
2017-09-17 15:17:50 +0300

    Versiyon 3

---
+++ TelefonDefteri/AkilliTelefon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelefonDefteri
+{
+    class AkilliTelefon
+    {
+        public string TelefonModeli { get; set; }
+    }
+}
